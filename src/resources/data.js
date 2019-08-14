5b419ee974f65c987b7077ef5693c0306f894885
Adam Codrington <adamcodrington95@gmail.com>
2019-08-14 20:32:16 +0100

    quiche v0.0

--- src/resources/data.js
+++ src/resources/data.js
@@ -636,6 +636,45 @@ export const recipes = [
     ],
     makes: "6 portions",
     hide: true
+  },
+  {
+    title: "quiche",
+    ingredients: [
+      "375g shortcrust pastry (or enough to line quiche tin)",
+      "240g salmon (skinless, boneless)",
+      "100ml milk",
+      "25g butter",
+      "1 white onion",
+      "2 garlic cloves",
+      "100g asparagus",
+      "75g frozen petit pois",
+      "200ml crème fraîche",
+      "4 large beaten eggs (1 for glaze, rest for filling)",
+      "1 tbsp fresh dill (no stalk)",
+      "1 tbsp mixed herbs",
+      "1 tsp salt",
+      "1 tsp ground black pepper",
+      "0.5 lemon (zest + juice)",
+      "50g mature cheddar (finely grated)"
+    ],
+    method: [
+      "roll out pastry into shape that will cover quiche tin",
+      "carefully place pastry over tin, ensuring all sides covered",
+      "gently press pastry into sides of tin (use ball of floured pastry)",
+      "prick base with fork",
+      "blind bake (with baking paper and beads) for 15 minutes",
+      "remove baking paper and beads, glaze with beaten egg",
+      "bake for further 15 minutes",
+      "poach salmon in milk (bring to simmer on medium heat, leave for 5 minutes)",
+      "separate milk and salmon, leave each to cool",
+      "lightly fry onion, garlic in butter",
+      "blanch petit pois, asparagus (boil for 3 minutes then cool rapidly)",
+      "combine all cooled ingredients to form filling",
+      "pour filling all over pastry",
+      "bake for 45-60 minutes at 180 " +
+        degreesCelcius +
+        " (until filling is set)"
+    ]
   }
   // sort by recipe title
 ].sort((a, b) => (a.title > b.title ? 1 : -1));
